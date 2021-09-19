45d2b583840dcd6135ec78b0c4dcb1f0a725c2a5
Светлана <55839939+GetsuDer@users.noreply.github.com>
2021-09-19 12:53:42 +0300

    Lecture 5 Exercise 1
    Not quite sure, mb not residual calculations

---
+++ print-tree-by-level.rkt
@@ -0,0 +1,47 @@
+#lang scheme
+(require racket/vector)
+
+; tree lib
+(define empty-tree #())
+(define make-tree vector)
+(define (tree-data tree) (vector-ref tree 0))
+(define (tree-left tree) (vector-ref tree 1))
+(define (tree-right tree) (vector-ref tree 2))
+(define (empty-tree? t) (equal? t #()))
+
+(define  (print-tree-by-level-desc tree)
+  (define (print-parentless-list lst)
+    (cond ((null? lst) (void))
+          (else
+           (printf "~a " (car lst))
+           (print-parentless-list (cdr lst))
+           )
+          )
+    )
+  (define (merge lst1 lst2)
+    (cond ((null? lst1) lst2)
+          ((null? lst2) lst1)
+          (else (cons (append (car lst1) (car lst2)) (merge (cdr lst1) (cdr lst2))))
+        )
+    )
+  (define (print-tree lst-of-lsts)
+    (cond ((null? lst-of-lsts) (void))
+           (else
+            (print-parentless-list (car lst-of-lsts))
+            (newline)
+            (print-tree (cdr lst-of-lsts))
+            )
+           )
+    )
+  (print-tree (reverse
+   (let loop ((res `()) (tail tree))
+    (cond ((empty-tree? tail) `())
+          (else
+           (cons (list (tree-data tail)) (merge (loop res (tree-right tail)) (loop res (tree-left tail))))
+           )
+          )
+    )
+   )
+              )
+  
+  )
